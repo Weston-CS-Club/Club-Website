68a4b9611adb4140618ca208e1d37b3c78fe85ea
Ben Rosenfeld <86892462+Benjamin-Rosenfeld@users.noreply.github.com>
2024-01-16 20:18:43 -0500

    Update main.js for terminal effect

--- main.js
+++ main.js
@@ -15,3 +15,31 @@ function enterN(element){
     element.style.color = "var(--error)"
     slash.style.color = "var(--error)"
 }
+
+// Special magic making cursor look like terminal
+$("#term-line").customCaret();
+let termline = $("#term-line")
+
+// If enter key is pressed while focused on terminal text line, run command on that line
+termline.on('keyup', function (e) {
+    if (e.key === 'Enter' || e.keyCode === 13) {
+        let innertex = termline.html();
+        enterLine(innertex);
+    }
+}
+
+);
+
+// Given a "terminal" command, run it
+function enterLine(command){
+    command = command.replace('<span id="caret"></span>','')
+    command = command.replace(/\uFEFF/g,"");
+
+    // Listing the terminal commands
+    if (command==="y") {
+        console.log("Yes Entered")
+    } 
+    if (command==="n") {
+        console.log("No Entered")
+    }
+}
